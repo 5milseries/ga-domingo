f6fae469a8e170505c3d0dab059f6cd5fd24a6c6
5milseries <167837718+5milseries@users.noreply.github.com>
2024-04-27 16:00:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -245,7 +245,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//pl23135811.highcpmgate.com/bb/fd/0a/bbfd0af697c2b1fab5cc55fe51dac04f.js';
+script.src = '//pl21546160.highcpmgate.com/6c/9c/ca/6c9cca2344310fc5890c46b7c2e6c2ca.js';
 
 // Agregar el elemento <script> al final del body
 document.body.appendChild(script);
